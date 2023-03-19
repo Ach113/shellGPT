8aa60d838c9e4e975b1471ad006c4f20e7029fc0
ach113 <archieberidze@gmail.com>
2023-03-19 12:34:48 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='shellGPT',
+    version='0.1.0',
+    description='chatGPT interface inside terminal',
+    url='https://github.com/Ach113/shellGPT',
+    author='Archil Beridze',
+    author_email='archil.beridze@sjsu.edu',
+    license='MIT',
+    install_requires=['openai~=0.27.2'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: User Interfaces'
+    ],
+    packages=['src'],
+    entry_points={
+        'console_scripts': ['shellgpt=src.main:main'],
+    },
+)
