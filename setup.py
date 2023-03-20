f5e42507b8e9e0d1b6a48a4eb6311aa157575068
ach113 <archieberidze@gmail.com>
2023-03-19 18:06:14 -0700

    update dev status

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=['openai~=0.27.2'],
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: User Interfaces'
